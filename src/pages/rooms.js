6cc9a37f4c24d2bd2406f1931175d4bd0656a64b
Gemuel Cornillez <gemueldajaycornillez152003@gmail.com>
2025-05-15 22:26:09 +0800

    Fix booking tab not showing anything

--- src/pages/rooms.js
+++ src/pages/rooms.js
@@ -1,17 +1,3 @@
-import React from 'react';
-
-const RoomsPage = () => {
-  return (
-    <div className="rooms-page">
-      <h1>Our Rooms</h1>
-      <p>Browse our selection of available rooms for your next visit.</p>
-      
-      {/* Room listings will be added here */}
-      <div className="rooms-container">
-        <p>Room listings coming soon...</p>
-      </div>
-    </div>
-  );
-};
+import RoomsPage from './rooms/RoomsPage.jsx';
 
 export default RoomsPage;
--- src/pages/rooms/index.js
+++ src/pages/rooms/index.js
@@ -1,1 +1,1 @@
-export { default } from './RoomsPage';
+export { default } from './RoomsPage.jsx';
